b22e79d269bb1505696c9a1e541c4e76a3d17eac
pedro.venturini <pedro.venturini@LCET1107.cwbdis.pucpr.br>
2016-03-23 19:41:46 -0300

    Lab 03

---
+++ 01-helloworld/src/main/java/br/com/venturini/prog4/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package br.com.venturini.prog4.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/ola")
+	public String hello(){
+		return "hello";
+	}
+
+}
